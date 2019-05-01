7301ea448fef3913156c1192257bd2d35352065a
James Dawson <SpaceMeerkat@github.com>
2019-05-01 09:25:53 +0100

    dataset generator added

--- explore_data.py
+++ explore_data.py
@@ -17,17 +17,28 @@ from tqdm import tqdm
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 
 #=============================================================================#
 #/////////////////////////////////////////////////////////////////////////////#
 #=============================================================================#
 
-class ARIEL:
+class ARIEL_data:
+    
+    def __init__(self,bin_width=None,percentile_path=None):
+        self.bin_width = bin_width
+        self.percentile_path = percentile_path
     
     def filename_collector(self, initial_path):
         f = []
         for filename in tqdm(glob.glob(initial_path+'*.txt')):
             f.append(os.path.basename(filename))
+        return f
+        
+    def filename_collector_long(self, initial_path):
+        f = []
+        for filename in tqdm(glob.glob(initial_path+'*.txt')):
+            f.append(filename)
         return f
     
     def data_loader(self,file_path,bin_width):
@@ -37,11 +48,6 @@ class ARIEL:
         _df_content = _content.groupby(pd.cut(_content.index,_dt)).mean()
         _df_content.index = _dt[:-1] + (_step/2)
         return _df_content
-        
-    def target_loader(self,file_path):
-        _content = np.loadtxt(file_path,delimiter='\t')
-        _content = pd.DataFrame(_content.T)
-        return _content
     
     def lightcurve(self,fluxes,save_path):
         plt.figure()
@@ -49,28 +55,143 @@ class ARIEL:
         plt.xlabel('t'); plt.ylabel('Relative brightness');
         plt.tight_layout()
         plt.savefig(save_path+'test.png')
+        
+    def make_image(self,file_path,save_path,plot=None):
+        _df = self.data_loader(file_path,self.bin_width)
+        _data = _df.values.astype(float).T
+        if self.percentile_path is not None:
+            _percentiles = pd.read_pickle(self.percentile_path).values
+            _data -= _percentiles
+            _data /= (1-_percentiles)
+        _data[_data!=_data] = 1
+        if plot is not None:
+            plt.figure(); plt.imshow(_data); plt.colorbar();
+            plt.gca().invert_yaxis()
+            plt.savefig(save_path+os.path.basename(file_path)+'.png')
+            
+    def get_all_data(self,file_path):
+        _df_content = self.data_loader(file_path,self.bin_width).values.T
+        return _df_content
+        
+    def pool_session(self,file_paths,processes,save_path):
+        pool = mp.Pool(processes = processes)
+        results = list(tqdm(pool.imap(self.get_all_data,file_paths),total=len(file_paths)))
+        results = [x for x in results if str(x) != 'nan']
+        df = pd.DataFrame(np.hstack(results))
+        _percentiles = np.nanpercentile(df.values,1,axis=1)
+        _df_percentiles = pd.DataFrame(_percentiles.T)
+        _df_percentiles.columns = ['PERCENTILE']
+        _df_percentiles.to_pickle(save_path+'channel_percentiles.pkl')
 
 #=============================================================================#
 #/////////////////////////////////////////////////////////////////////////////#
 #=============================================================================#
 
-#_path = "/home/anubis/c1307135/ARIEL/noisy_test/"
-#_filenames = ARIEL().filename_collector(initial_path=_path)
-#_low_noise = []
-#for i in _filenames:
-#    i = i[:-4]
-#    if i[-2:]=='01' and i[-5:-3]=='01':
-#        _low_noise.append(i+'.txt')
-#_file_name = _low_noise[1]
-#_df = ARIEL().data_loader(file_path=_path+_file_name, bin_width=5)
-#_save_path = '/home/corona/c1307135/Holy_Grail/test_images/'
-#ARIEL().lightcurve(fluxes=_df[1],save_path=_save_path)
+class ARIEL_targets:
+    
+    def filename_collector(self, initial_path):
+        f = []
+        for filename in tqdm(glob.glob(initial_path+'*.txt')):
+            f.append(filename)
+        return f
 
-_target_path = '/home/anubis/c1307135/ARIEL/params_train/'
-_target_files = ARIEL().filename_collector(initial_path=_target_path)
-_targets = ARIEL().target_loader(_target_path+_target_files[0])
-print(_targets)
-
+    def target_loader(self,file_path):
+        _content = np.loadtxt(file_path,delimiter='\t')
+        _content = pd.DataFrame(_content.T)
+        _content.columns = ['LABEL']
+        return _content     
+        
+    def target_distribution(self,filenames,wavelength,save_path=None):
+        _targets = []
+        for _filename in tqdm(filenames,total=len(filenames)):
+            _content = self.target_loader(_filename)
+            _target = _content.iloc[wavelength].values.astype(float)
+            _targets.append(_target)
+        if  save_path is not None:
+            _df_targets = pd.DataFrame(_targets)
+            _df_targets.columns = ['LABEL']
+            _df_targets.to_pickle(save_path+'targets_wavelength_'+str(wavelength)+'.pkl')
+        return _targets
+        
 #=============================================================================#
 #/////////////////////////////////////////////////////////////////////////////#
 #=============================================================================#
+        
+        
+class ARIEL_dataset_creator:
+    
+    def __init__(self,data_path,target_path,save_path,bin_width,percentile_path,percentiles=None):
+        self.data_path = data_path
+        self.target_path = target_path
+        self.save_path = save_path
+        self.bin_width = bin_width
+        self.percentile_path = percentile_path
+        self.percentiles = percentiles
+    
+    def percentile_loader(self):
+        self.percentiles = pd.read_pickle(self.percentile_path).values
+        
+    def filename_collector(self):
+        f = []
+        for filename in tqdm(glob.glob(self.data_path+'*.txt')):
+            f.append(os.path.basename(filename))
+        return f
+    
+    def image_maker(self,file_path):
+        if self.percentiles is None:
+            self.percentile_loader()
+        _content = np.loadtxt(self.data_path+file_path,delimiter='\t').T
+        _content = pd.DataFrame(_content); _content[_content.iloc[:,:]>1]=np.nan;
+        _step = self.bin_width; _dt = np.arange(0,301,_step);
+        _df_content = _content.groupby(pd.cut(_content.index,_dt)).mean()
+        _df_content.index = _dt[:-1] + (_step/2)
+        _data = _df_content.values.astype(float).T
+        _data -= self.percentiles; _data /= (1-self.percentiles);
+        _data[_data!=_data] = 1
+        return _data.T
+        
+    def target_loader(self,file_path):
+        _content = np.loadtxt(self.target_path+file_path,delimiter='\t')
+        return _content 
+            
+    def make_all_data(self,_file):
+        _image = self.image_maker(_file)
+        _target = self.target_loader(_file)
+        _grouped_data = np.vstack([_target,_image])
+        _df_grouped_data = pd.DataFrame(_grouped_data.T)
+        _df_grouped_data.to_pickle(self.save_path+_file[:-4]+'.pkl')
+        return
+        
+    def pool_session(self,processors):
+        _file_directories = self.filename_collector()
+        pool = mp.Pool(processes = processors)
+        results = list(tqdm(pool.imap(self.make_all_data,_file_directories),total=len(_file_directories)))
+        results = [x for x in results if str(x) != 'nan']
+            
+   
+#=============================================================================#
+#/////////////////////////////////////////////////////////////////////////////#
+#=============================================================================#   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ script_runner.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 09:33:39 2019
+
+@author: c1307135
+"""
+#=============================================================================#
+#/////////////////////////////////////////////////////////////////////////////#
+#=============================================================================#
+
+from explore_data import ARIEL_data, ARIEL_targets, ARIEL_dataset_creator
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+#=============================================================================#
+#/////////////////////////////////////////////////////////////////////////////#
+#=============================================================================#
+
+lc_plotter = False
+if lc_plotter == True:
+    _path = "/home/anubis/c1307135/ARIEL/noisy_test/"
+    _filenames = ARIEL_data().filename_collector(initial_path=_path)
+    _low_noise = []
+    for i in _filenames:
+        i = i[:-4]
+        if i[-2:]=='01' and i[-5:-3]=='01':
+            _low_noise.append(i+'.txt')
+    _file_name = _low_noise[1]
+    _df = ARIEL_data().data_loader(file_path=_path+_file_name, bin_width=5)
+    print(_df.shape)
+    _save_path = '/home/corona/c1307135/Holy_Grail/test_images/'
+    ARIEL_data().lightcurve(fluxes=_df[0],save_path=_save_path)
+
+data_image = False
+if data_image == True:
+    _path = "/home/anubis/c1307135/ARIEL/noisy_test/"
+    _save_path = '/home/corona/c1307135/Holy_Grail/test_images/'
+    _filenames = ARIEL_data().filename_collector(initial_path=_path)   
+    ARIEL_data().make_image(_path+_filenames[0],bin_width=10,save_path=_save_path,plot=True)
+    
+get_all_data = False
+if get_all_data == True:
+    _path = "/home/anubis/c1307135/ARIEL/noisy_train/"
+    _save_path = '/home/corona/c1307135/Holy_Grail/test_images/'
+    _filenames = ARIEL_data().filename_collector_long(initial_path=_path)   
+    ARIEL_data(bin_width=10).pool_session(_filenames,processes=14,save_path=_save_path)
+    
+data_image_norm = False
+if data_image_norm == True:
+    _path = "/home/anubis/c1307135/ARIEL/noisy_train/"
+    _perc_path = '/home/corona/c1307135/Holy_Grail/test_images/channel_percentiles.pkl'
+    _save_path = '/home/corona/c1307135/Holy_Grail/test_images/normed/'
+    _filenames = ARIEL_data().filename_collector_long(initial_path=_path) 
+    ARIEL_data(bin_width=5,percentile_path=_perc_path).make_image(_filenames[2],save_path=_save_path,plot=True)    
+
+target_grabber = False
+if target_grabber == True:
+    _target_path = '/home/anubis/c1307135/ARIEL/params_train/'
+    _target_files = ARIEL_targets().filename_collector(initial_path=_target_path)
+    _targets = ARIEL_targets().target_loader(_target_path+_target_files[0])
+    print(_targets.iloc[54].values)
+    
+target_dists = False
+if target_dists == True:
+    _target_path = '/home/anubis/c1307135/ARIEL/params_train/'
+    _save_path = '/home/corona/c1307135/Holy_Grail/test_images/'
+    _target_files = ARIEL_targets().filename_collector(initial_path=_target_path)
+    _distribution_1 = ARIEL_targets().target_distribution(_target_files,54,save_path=_save_path) 
+
+_path = "/home/anubis/c1307135/ARIEL/noisy_train/"
+_target_path = '/home/anubis/c1307135/ARIEL/params_train/'
+_save_path = '/home/anubis/c1307135/ARIEL/TRAIN_FLUX/'
+_perc_path = '/home/corona/c1307135/Holy_Grail/test_images/channel_percentiles.pkl'
+ARIEL_dataset_creator(data_path=_path,target_path=_target_path,save_path=_save_path,
+                      bin_width=5,percentile_path=_perc_path).pool_session(processors=14)
+    
